2890076a0df5f0dacaa3dcde0bec87c03449b4b9
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-06 15:15:56 +0530

    We'll fix it after a reboot.

--- jugnoo/src/main.cpp
+++ jugnoo/src/main.cpp
@@ -18,6 +18,7 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 
 using namespace std;
 using namespace cv;
@@ -30,5 +31,18 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    Mat image;
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+
+    if(! image.data )                              // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
+    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );                   // Show our image inside it.
+
+    waitKey(0);                                        
     return 0;
 }
