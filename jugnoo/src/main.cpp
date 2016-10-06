b563629d0917b8679a2c173fb9a92d3b4e92ccf1
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-06 19:54:54 +0530

    Using opencv mat.

--- jugnoo/src/main.cpp
+++ jugnoo/src/main.cpp
@@ -19,34 +19,32 @@
 #include <iostream>
 #include <vector>
 #include <array>
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/graph/graph_traits.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/dijkstra_shortest_paths.hpp>
 #include <tuple>
 #include <valarray>
 #include <numeric>
-
+#include <opencv2/opencv.hpp>
 #include "tiffio.h"
-#include "image.hpp"
 
 using namespace std;
-using namespace boost::numeric;
+using namespace cv;
 
 typedef unsigned short pixal_type_t;
-typedef ublas::matrix< pixal_type_t > image_type_t;
-typedef tuple<size_t, size_t> index_type_t;
+typedef tuple<int, int> index_type_t;
 
 //typedef adjacency_list<vecS, vecS, bidirectionalS> Graph;
 
 void get_timeseries_of_pixal( 
-        const vector< image_type_t> & frames
+        const vector< Mat > & frames
         , index_type_t index
         , vector< pixal_type_t >& values
         )
 {
     for ( auto image : frames )
-        values.push_back( image( get<0>(index), get<1>(index) ) );
+    {
+
+        pixal_type_t val = image.at<pixal_type_t>( get<0>(index), get<1>(index) );
+        values.push_back( val );
+    }
 }
 
 void smooth( const vector< pixal_type_t > signal
@@ -74,19 +72,20 @@ void correlate( const vector< pixal_type_t> a, const vector<pixal_type_t> b)
 {
 }
 
-void compute_correlation( const vector< image_type_t >& frames )
+void compute_correlation( const vector< Mat >& frames )
 {
     auto frame0 = frames[0];
     size_t rows, cols;
-    cols = frame0.size1(); rows = frame0.size2( );
+    cols = frame0.cols; rows = frame0.rows;
+
     std::cout << "Rows " << rows << " cols : " << cols << std::endl;
 
     vector< vector< pixal_type_t > > time_series;
 
     // create the list of indices, I need to iterate over.
     vector< tuple<size_t, size_t> > indices;
-    for( size_t i = 0; i < rows; i++)
-        for( size_t ii = 0; i < cols; i++ )
+    for( int i = 0; i < rows; i++)
+        for( int ii = 0; i < cols; i++ )
             indices.push_back( make_tuple( i, ii ) );
 
     int count = 0;
@@ -101,7 +100,7 @@ void compute_correlation( const vector< image_type_t >& frames )
             get_timeseries_of_pixal( frames, j, pixalB);
             vector< pixal_type_t > convolution;
             convolve( pixalA, pixalB, convolution);
-            cout << "," << accumulate( convolution.begin(), convolution.end(), 0);
+            //cout << "," << accumulate( convolution.begin(), convolution.end(), 0);
         }
     cout << "Total pixals " << count << endl;
 }
@@ -115,7 +114,7 @@ int main(int argc, char *argv[])
     }
 
     TIFF *tif = TIFFOpen( argv[1], "r");
-    std::vector< image_type_t > frames;
+    std::vector< Mat > frames;
 
     if (tif) {
 	int dircount = 0;
@@ -131,14 +130,13 @@ int main(int argc, char *argv[])
             TIFFGetField( tif, TIFFTAG_IMAGELENGTH, &h);
             npixals = w * h;
 
-            image_type_t image( h, w );
-
+            Mat image( h, w, CV_8UC1 );
             for (size_t i = 0; i < h; i++) 
             {
                 pixal_type_t* row =  new pixal_type_t[ w ];
                 TIFFReadScanline( tif, row, i, 0 );
                 for (size_t ii = 0; ii < w; ii++) 
-                    image(i, ii) = row[ii];
+                    image.at<uchar>(i, ii) = row[ii];
                 delete row;
             }
 
