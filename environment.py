a4981b6241aa0f5d2ee8581c7a7e4e40d92250fe
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-08 19:40:51 +0530

    This is better. I got 120 cells.

---
+++ environment.py
@@ -0,0 +1,37 @@
+"""environment.py: 
+
+All global variables and stuff reside here.
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2015, Dilawar Singh and NCBS Bangalore"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import logging
+
+class Args(): pass
+args_ = Args()
+
+shape_ = None
+images_ = {}
+save_direc_ = None
+rois_ = []
+
+logging.basicConfig(level=logging.DEBUG,
+    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%m-%d %H:%M',
+    filename='__log__.log',
+    filemode='w')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+_logger = logging.getLogger('')
+_logger.addHandler(console)
+
+
--- locate_roi.py
+++ locate_roi.py
@@ -12,18 +12,26 @@ __status__           = "Development"
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig
+import scipy.ndimage as simg
 import os
 import sys
 import time
 import cv2
 import image_reader as imgr
-import igraph as ig
+import environment as e
+# import igraph as ig
+import networkx as nx
 
 import logging
 logger = logging.getLogger('')
 
-g_ = ig.Graph( )
+# g_ = ig.Graph( )
+g_ = nx.Graph( )
+cell_ = nx.Graph( )
 indir_ = None
+pixalcvs_ = []
+template_ = None
+
 
 def plot_two_pixals( a, b, window = 3 ):
     plt.figure( )
@@ -51,22 +59,71 @@ def activity_corr( a, b ):
     ab = convolve( a, b )[0:N]
     return np.sum( self / ab ) / N
 
-def build_correlation_graph( graph, frames ):
-    logger.info( 'Total pixals %d' % ( graph.vcount() ** 2 ))
-    for i, m in enumerate(graph.vs( )):
-        logger.info( 'Done nodes %d (out of %d)' % (i, graph.vcount() ) )
-        for n in graph.vs( ):
-            mname, nname = m['name'], n['name']
-            if m == n: 
+def distance( p1, p2 ):
+    x1, y1 = p1
+    x2, y2 = p2 
+    return (( x1 - x2 ) ** 2.0 + (y1 - y2 ) ** 2.0 ) ** 0.5
+
+def is_connected( m, n, img ):
+    """ 
+    If pixal m and pixal n have path between them, return true.
+    """
+    line = simg.map_coordinates(img, [ m, n ] )
+    if line.min() < 1:
+        return False
+    return True
+
+def build_cell_graph( graph, frames ):
+    """ Build a cell graph """
+    global cell_ 
+    global pixalcvs_
+    global template_
+    nodesToProcess = sorted( pixalcvs_ , reverse = True )
+    cells = []
+    img = np.int32( np.sum( frames, axis = 2 ))
+    for (c, n) in nodesToProcess:
+        if graph.node[n].get('cell', None) is not None:
+            continue
+        graph.node[n]['cell'] = len(cells)
+        cells.append( n )
+        for m in graph.nodes( ):
+            if distance(m, n ) > 20.0:
                 continue 
-            (m1, m2), (n1, n2) = eval(mname), eval(nname)
-            pixalsM, pixalsN = frames[m1,m2,:], frames[n1,n2,:]
+            # Accept all pixals in neighbourhood 
+            if distance(m, n) < 5.0 or is_connected(m, n, template_):
+                graph.node[m]['cell'] = len( cells )
+
+    print( '[INFO] Total cells %d' % len(cells) )
+    for p in cells:
+        cv2.circle( template_, (p[1], p[0]), 2, 20 );
+    plt.figure( )
+    plt.imshow( template_, interpolation = 'none', aspect = 'auto' )
+    plt.colorbar( )
+    plt.savefig( 'cells.png' )
+    print( img.shape )
+
+
+def build_correlation_graph( graph, frames ):
+    logger.info( 'Total pixals %d' % ( graph.number_of_nodes() ** 2 ))
+    return 
+
+    for i, m in enumerate(graph.nodes( )):
+        logger.info( 'Done nodes %d (out of %d)' % (i, graph.number_of_nodes() ) )
+        for n in graph.nodes( ):
+            if distance( m, n ) < 10:
+                continue 
+            print( m, n, distance( m, n ) )
+            # mname, nname = m['name'], n['name']
+            # if m == n: 
+                # continue 
+            # (m1, m2), (n1, n2) = eval(mname), eval(nname)
+            # pixalsM, pixalsN = frames[m1,m2,:], frames[n1,n2,:]
             # plot_two_pixals( pixalsM, pixalsN )
-            corr = activity_corr( pixalsM, pixalsN )
-            graph.add_edge( m, n, weight=corr )
-            logger.debug( 'Computed corr between %s and %s = %s' % ( 
-                mname, nname, corr) 
-                )
+            # corr = activity_corr( pixalsM, pixalsN )
+            # graph.add_edge( m, n, weight=corr )
+            # logger.debug( 'Computed corr between %s and %s = %s' % ( 
+                # mname, nname, corr) 
+                # )
     logger.info( 'Done building graph. Now extract communities' )
     outfile = os.path.join( e.args_.input, 'activity_correlation.gml' )
     ig.write( graph, outfile, format = 'graphml' )
@@ -74,6 +131,7 @@ def build_correlation_graph( graph, frames ):
 
 def process_input( ):
     global g_
+    global template_
     inputdir = e.args_.input
     tiffs = []
     for d, sd, fs in os.walk( inputdir ):
@@ -87,24 +145,27 @@ def process_input( ):
         frames = imgr.read_frames( inputfile )
         allFrames += frames 
 
-    template = np.zeros( shape = allFrames[0].shape )
+    template_ = np.zeros( shape = allFrames[0].shape )
     frames = np.dstack( allFrames )
     for i in range(frames.shape[2]):
         f = frames[:,:,i]
-    for (r,c), val in np.ndenumerate( template ):
+    for (r,c), val in np.ndenumerate( template_ ):
         pixals = frames[r,c,:]
-        if pixals.var( ) > 300.0:
-            template[r,c] = pixals.mean( )
-            g_.add_vertex( name=str((r,c)) )
+        cv = pixals.var( ) / pixals.mean( ) 
+        # if pixals.max() >= pixals.mean() + 4 * pixals.std():
+        if cv > 4.0:
+            template_[r,c] = pixals.mean( )
+            g_.add_node( (r,c), cv = cv )
+            pixalcvs_.append( (cv, (r,c) ) )
 
-    build_correlation_graph( g_, frames )
     plt.subplot(2, 1, 1 )
-    plt.imshow( template )
+    plt.imshow( template_ )
     plt.colorbar( )
     plt.subplot(2, 1, 2 )
     plt.imshow( np.sum( frames, axis = 2 ) )
     plt.savefig( "template.png" )
     print( 'Saved to template.png' )
+    build_cell_graph( g_, frames )
 
     
 def main( ):
