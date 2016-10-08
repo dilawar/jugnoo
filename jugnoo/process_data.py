27de3cc3a493fc883f35fbcc7e627d28d0d42273
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-08 15:57:43 +0530

    Using igraph to build graph.

--- jugnoo/process_data.py
+++ jugnoo/process_data.py
@@ -14,14 +14,42 @@ __email__            = "dilawars@ncbs.res.in"
 __status__           = "Development"
 
 import sys
+import re
 import os
 import matplotlib.pyplot as plt
 import igraph as ig
 
+nodes_ = [ ]
+g_ = ig.Graph( )
+
+def add_node( nodename ):
+    nodeId = 0
+    if nodename not in nodes_:
+        nodes_.append( nodename )
+        nodeId = len( nodes_ )
+        g_.add_vertex( nodeId, label = nodename )
+        return nodeId
+    nodeId = nodes_.index( nodename )
+    return nodeId
+
+
+def build_graph( filename ):
+    with open( filename, 'r' ) as f:
+        lines = f.read().split( '\n' )
+    edges = filter( lambda l : '->' in l, lines )
+    edgePat = re.compile( r'(?P<src>.+?)->(?P<tgt>.+?)\[(?P<attr>.*?)\]', re.DOTALL )
+    for e in edges:
+        m = edgePat.search( e )
+        src, tgt = m.group( 'src' ), m.group( 'tgt' )
+        srcId, tgtId = add_node( src ), add_node( tgt )
+        attr = m.group( 'attr' )
+    print( 'Total nodes %d' % len( nodes_ ) )
+
 def main():
     """docstring for main"""
-    g = ig.load( sys.argv[1] )
-    print( g)
+    g = build_graph( sys.argv[1] )
+    print( g )
+
 
 if __name__ == '__main__':
     main()
