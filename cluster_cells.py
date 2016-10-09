4c2efa64a0e1c3ec110ae3d66bf3c5e0a7b07d83
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-09 19:24:12 +0530

    Now I can create both cluster and timeseries plots.

--- cluster_cells.py
+++ cluster_cells.py
@@ -22,7 +22,7 @@ def main( graphfile ):
     cells = nx.read_gpickle( graphfile )
     timeseries_ = cells.graph['timeseries']
     syncDict = defaultdict( list )
-    syncImg = np.zeros( shape = (210,150) )
+    syncImg = np.zeros( shape = cells.graph['shape'] )
     for i, c in enumerate( nx.attracting_components( cells ) ):
         if len(c) < 2:
             continue
@@ -31,6 +31,16 @@ def main( graphfile ):
             cv2.circle( syncImg, (p[1], p[0]), 2, (i+1), 2 )
             syncDict[str(c)].append( cells.node[p]['timeseries'] )
 
+    plt.subplot( 2, 1, 1)
+    img = [ ]
+    for k in syncDict:
+        img += syncDict[k]
+        print img[0].size
+        img.append( np.zeros( img[0].size ) )
+
+    plt.imshow( np.vstack( img ), interpolation = 'none', aspect = 'auto' )
+
+    plt.subplot( 2, 1, 2 )
     plt.imshow( syncImg, interpolation = 'none', aspect = 'auto' )
     plt.savefig( 'clusters.png' )
 
