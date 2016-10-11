24e492d13e52d870f8df65baf56781f924eb470c
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-11 19:08:09 +0530

    This is a good script.

--- motion_correct.py
+++ motion_correct.py
@@ -16,22 +16,30 @@ __email__            = "dilawars@ncbs.res.in"
 __status__           = "Development"
 
 import sys
+import shutil
 import os
-import cv2
+
+data_ = '_sima.sima' 
+
+if os.path.isdir( data_ ):
+    print( '[INFO] removing dir %s' % data_ )
+    shutil.rmtree( data_ )
 
 def use_sima( tifffile ):
     import sima.motion
-    outfile = [[[ '%s_corrected.tif' % tifffile ]]]
-    approach = sima.motion.PlaneTranslation2D( max_displacement=[20,20] )
+    outfile = '%s_corrected.tif' % tifffile
+    # approach = sima.motion.PlaneTranslation2D( max_displacement=[20,20] )
+    approach = sima.motion.HiddenMarkov2D( 
+            granularity='row', max_displacement=[20, 30], verbose=False
+            )
     seqs = [ sima.Sequence.create( 'TIFF', tifffile ) ]
-    dataset = approach.correct( seqs, '_sima.sima' )
+    dataset = approach.correct( seqs, data_ )
     try:
-        dataset.export_frames( outfile, fmt = 'TIFF16' )
+        dataset.export_frames( [[[ outfile ]]], fmt = 'TIFF16' )
     except Exception as e:
         print( '[WARN] Failed to correct motion. Error was %s' % e )
-        os.rmdir( '_sima' )
         quit( )
-    os.rmdir( '_sima.sima' )
+
     print( '[INFO] Wrote corrected file to %s' % outfile )
 
 def main( datafile ):
